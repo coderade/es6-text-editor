0913e7ac3ccbfe16d39677fd02221ed6757afe7c
Valdeci Gomes <coderade@outlook.com>
2017-09-18 20:48:58 -0300

    Add styled components, add layout to the editor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Markup, Editor, Container, Column, Row, RuleInput, RuleLabel, StyleInput, Button, Document} from "./styled";
 
 class App extends Component {
-  render() {
-    return (
-      <div>ES6 Text editor</div>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Column>
+                    <Button>
+                        New Rule
+                    </Button>
+                </Column>
+                <Column>
+                    <Button>
+                        Random Text
+                    </Button>
+                    <Document>
+                        <Editor/>
+                        <Markup/>
+                    </Document>
+                </Column>
+            </Container>
+        );
+    }
 }
 
 export default App;
