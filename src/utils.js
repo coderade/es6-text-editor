c50c973563a7bc7badb329838857a2982d3352f3
Valdeci Gomes <coderade@outlook.com>
2017-09-18 23:17:02 -0300

    Add the Utils file to call the Shakspeare api

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+const shakespeareApi = "https://api.graph.cool/simple/v1/shakespeare";
+
+let options = () => {
+    return {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            //TODO
+        })
+    }
+};
